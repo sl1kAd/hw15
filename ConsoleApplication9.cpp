3a1b9c68f94f9d233c8a2f5307d21ba8f2ff2457
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2022-03-02 16:04:28 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int Ex1(unsigned char* str, unsigned int len)
+{
+    unsigned int seed = 131313;
+
+    unsigned int hash = 0;
+
+    unsigned int i = 0;
+
+    for (i = 0; i < len; str++, i++)
+    {
+        hash = (hash * seed) + *str + i;
+    }
+    return hash;
+}
+
+int Ex2(int *coin, const int n, const int maxN) 
+{
+
+    int arr[n];
+
+    clearIntArr(arr, n);
+
+    int sum = 0;
+
+    int o = 0;
+
+    for (int i = 0; i < maxN; i++) 
+    {
+        if ((coin[i] * 2) < 98) 
+        {
+            sum += coin[i];
+
+            o++;
+        }
+
+        if (sum < 98) break;
+    }
+    return o;
+}
+
+void Ex21() 
+{
+    const int Cmax = 5;
+
+    const int maxN = 98;
+
+    int coin[] = { 50, 10, 5, 2, 1 };
+
+    cout << Ex2(coin, Cmax, maxN);
+}
+
